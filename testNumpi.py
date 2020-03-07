d67569cf15aae37a7729d650d83c4065f60f61c4
Esteban_RM71 <stefano.cremona@gmail.com>
2020-03-07 16:49:19 +0000

    Show data without filter

--- testNumpi.py
+++ testNumpi.py
@@ -1,19 +1,26 @@
+#!/c/Python27/python.exe
 import matplotlib.pyplot as plt
 import csv
+import sys
 
 #x_number_values = [1, 2, 3, 4, 5, 6, 7]
+maxValue=4 # 4 for Unity simulator, 22 real case
+
+# Parse the string values from first file reading
+def parseValues(x):
+  return float(str.strip(x)) if len(x) > 0 else maxValue # Last value in line is usually empty
 
 # Plot a line based on the x and y axis value list.
-def draw_line():
+def draw_line(path, prefix):
 
-    f = open("walkingStef.txt", "r")
+    f = open(path+prefix+"Left.txt", "r")
     f1 = f.read()
-    points = [map(lambda x: float(str.strip(x)), s_inner.split(',')) for s_inner in f1.splitlines()]
+    points = [map(parseValues, s_inner.split(',')) for s_inner in f1.splitlines()]
     f.close()
 
-    f = open("walkingStefRight.txt", "r")
+    f = open(path+prefix+"Right.txt", "r")
     f1 = f.read()
-    pointsRight = [map(lambda x: float(str.strip(x)), s_inner.split(',')) for s_inner in f1.splitlines()]
+    pointsRight = [map(parseValues, s_inner.split(',')) for s_inner in f1.splitlines()]
     f.close()
 
     if (len(points) == 0):
@@ -25,21 +32,21 @@ def draw_line():
     k = 0
     for i in points[0]:
         x_number_values.append(k)
-        if (k > 0 and (i > 200 or i <= 5)):
-            points[0][k] = points[0][k-1]
+        # if (k > 0 and (i > 200 or i <= 5)):
+        #     points[0][k] = points[0][k-1]
         k += 1
 
     # Normalise the Right points
-    k = 0
-    for i in pointsRight[0]:
-        if (k > 0 and (i > 200 or i <= 5)): 
-            pointsRight[0][k] = pointsRight[0][k-1]
-        k += 1
+    # k = 0
+    # for i in pointsRight[0]:
+    #     if (k > 0 and (i > 200 or i <= 5)): 
+    #         pointsRight[0][k] = pointsRight[0][k-1]
+    #     k += 1
 
     # Revert the right points
     k = 0
     for i in pointsRight[0]:
-        pointsRight[0][k] = (pointsRight[0][k] * 100 -19500) * -1 / 100
+        pointsRight[0][k] = (pointsRight[0][k] * 100 -maxValue*100) * -1 / 100
         k += 1
 
     print(points[0])
@@ -85,4 +92,4 @@ def draw_line():
     plt.show()
 
 if __name__ == '__main__':
-    draw_line()
+    draw_line("C:/Users/e7470/rowData/singleDouglasWalking" + "/", "202003062333044708")
