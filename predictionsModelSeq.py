9a9a43f6a8a0da030c3d4e225e14129ffab5fdd6
Esteban_RM71 <stefano.cremona@gmail.com>
2020-03-17 10:34:16 +0000

    Improve image display

--- predictionsModelSeq.py
+++ predictionsModelSeq.py
@@ -18,7 +18,7 @@ def plot_image(i, predictions_array, true_label, img):
   else:
     color = 'red'
 
-  plt.xlabel("{} {:2.0f}% ({})".format(CLASS_NAMES[int(predicted_label)],
+  plt.xlabel("{} {:2.0f}% \n ({})".format(CLASS_NAMES[int(predicted_label)],
                                 100*np.max(predictions_array),
                                 CLASS_NAMES[int(true_label)]),
                                 color=color)
@@ -26,7 +26,7 @@ def plot_image(i, predictions_array, true_label, img):
 def plot_value_array(i, predictions_array, true_label):
   predictions_array, true_label = predictions_array, true_label[i]
   plt.grid(False)
-  plt.xticks(range(len(CLASS_NAMES)))
+  plt.xticks(range(len(CLASS_NAMES)), rotation='vertical')
   plt.yticks([])
   thisplot = plt.bar(range(len(CLASS_NAMES)), predictions_array, color="#777777")
   plt.ylim([0, 1])
@@ -42,7 +42,7 @@ i = 15
 print(predictions[i])
 print(np.argmax(predictions[i]))
 
-plt.figure(figsize=(6,3))
+plt.figure(figsize=(12,6))
 plt.subplot(1,2,1)
 plot_image(i, predictions[i], test_labels, test_images)
 plt.subplot(1,2,2)
