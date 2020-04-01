e198e5433b55e5b990ba468409f35e419ace2922
Esteban_RM71 <stefano.cremona@gmail.com>
2020-04-01 17:49:12 +0100

    Test all the images in a directory

--- 05_liveTest.py
+++ 05_liveTest.py
@@ -7,6 +7,18 @@ This is a temporary script file.
 """
 # from saveImage import draw_line
 from livePrediction import predict
+import platform
+from os import listdir
+from os.path import join, isfile
 
+testDir = "C:/Users/e7470/rowData/testDowglas"
+
+imagesToEvaluate = [f for f in listdir(testDir) if isfile(join(testDir, f)) and f.rfind('FilledSquared') >= 0] # DowglasForward is a test Dir
+# print(imagesToEvaluate)
 # draw_line("/home/pi/Scripts" + "/", "", "")
-predict("/home/pi/Scripts", "FilledCropSquared.png")
+if platform.system() == "Windows":
+    for f in imagesToEvaluate:
+        print("Image: " + f)
+        predict(testDir, f)
+else:
+    predict("/home/pi/Scripts", "FilledCropSquared.png")
