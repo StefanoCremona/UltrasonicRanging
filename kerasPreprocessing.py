52f9421ba1a3d3dc15ff199aa03e624ed398aac1
Esteban_RM71 <stefano.cremona@gmail.com>
2020-04-01 17:51:59 +0100

    Remove logs and add utility functions

--- kerasPreprocessing.py
+++ kerasPreprocessing.py
@@ -11,37 +11,41 @@ import matplotlib.pyplot as plt
 import os
 import tensorflow as tf
 import pathlib
-import keras
+# import keras
 
-data_dir = pathlib.Path("C:/Users/e7470/data/train")
-test_dir = pathlib.Path("C:/Users/e7470/data/test")
+data_dir = pathlib.Path("C:/Users/e7470/dataTest/trainRowNormal")
+test_dir = pathlib.Path("C:/Users/e7470/dataTest/testRowNormal")
+models_dir = pathlib.Path("C:/Users/e7470/models")
 
 CLASS_NAMES = np.array([item.name for item in data_dir.glob('*')])
 # CLASS_NAMES = np.array([item.name for item in data_dir.glob('*') if item.name != "LICENSE.txt"])
 print('Labels: ' + str(CLASS_NAMES))
 
 image_count = len(list(data_dir.glob('*/*.png')))
-print('Total images: ' + str(image_count))
+print('Total images between training and test:' + str(image_count))
 
-roses = list(data_dir.glob(CLASS_NAMES[0] + '/*'))
-
-print("Samples from " + CLASS_NAMES[0])
-for image_path in roses[:3]:
-    display.display(Image.open(str(image_path)))
+def showSamples():
+    samples = list(data_dir.glob(CLASS_NAMES[0] + '/*'))
     
-roses = list(data_dir.glob(CLASS_NAMES[1] + '/*'))
+    print("Samples from " + CLASS_NAMES[0])
+    for image_path in samples[:3]:
+        display.display(Image.open(str(image_path)))
+        
+    samples = list(data_dir.glob(CLASS_NAMES[1] + '/*'))
+    
+    print("Samples from " + CLASS_NAMES[1])
+    for image_path in samples[:3]:
+        display.display(Image.open(str(image_path)))
 
-print("Samples from " + CLASS_NAMES[1])
-for image_path in roses[:3]:
-    display.display(Image.open(str(image_path)))
-
+# showSamples()
+# Function to voncert images to gray scale
 def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
     
 # The 1./255 is to convert from uint8 to float32 in range [0,1].
 image_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1./255)
 
-BATCH_SIZE = 100
+BATCH_SIZE = 1000
 IMG_HEIGHT = 500
 IMG_WIDTH = 500
 STEPS_PER_EPOCH = np.ceil(image_count/BATCH_SIZE)
@@ -64,7 +68,7 @@ test_data_gen = image_generator.flow_from_directory(directory=str(test_dir),
 
 # Inspect a batch:
 def show_batch(image_batch, label_batch, rangeN):
-  plt.figure(figsize=(5,5))
+  plt.figure(figsize=(10,10))
   for n in range(rangeN):
       ax = plt.subplot(5,5,n+1)
       plt.imshow(image_batch[n])
@@ -74,18 +78,19 @@ def show_batch(image_batch, label_batch, rangeN):
 train_images, train_labels = train_data_gen.next()
 test_images, test_labels = test_data_gen.next()
 
-print(type(train_images))
-print(train_images.shape)
-print(type(train_labels))
-print(train_labels)
+# print(type(train_images))
+# print(train_images.shape)
+# print(type(train_labels))
+# print(train_labels)
 
 # Convert in grayscale np array images. From 28*28*3 to 28*28 vector
 train_images = np.asarray([rgb2gray(pi) for pi in train_images])
 test_images = np.asarray([rgb2gray(pi) for pi in test_images])
 
-print(type(train_images))
-print(train_images.shape)
+# print(type(train_images))
+print('Train images shape: ' + str(train_images.shape))
 
+#print('Train and Test examples:')
 show_batch(train_images, train_labels, 25)
 show_batch(test_images, test_labels, 5)
 
