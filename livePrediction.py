19afa3612617625304f30bd970557b7b34a0560c
Esteban_RM71 <stefano.cremona@gmail.com>
2020-04-01 17:52:36 +0100

    Add windows switches

--- livePrediction.py
+++ livePrediction.py
@@ -4,10 +4,12 @@ from os.path import join
 from livePredictionUtils import getProbabilityModel, loadClassesNames, getImageToPlot, getPredictions, plotImgAndPrediction
 import platform
 
-models_dir = pathlib.Path("/home/pi/Models")
 if platform.system() == "Windows":
     models_dir = pathlib.Path("C:/Users/e7470/models")
-modelName = "Seq20200324222341AllFilled" # Type here the testName of the model you want to load
+else:
+    models_dir = pathlib.Path("/home/pi/Models")
+
+modelName = "20200401150306AllFilledSeq100" # Type here the testName of the model you want to load
 # imgDir = pathlib.Path("/home/pi/Images/oneNormT1")
 # imgName = 'Flipped202003101656127128Tsquared1.png'
 
@@ -17,7 +19,7 @@ except NameError:
     model_file = join(str(models_dir), 'model'+modelName+'.json')
     weights_file = join(str(models_dir), 'weights'+modelName+'.h5')
     probability_model = getProbabilityModel(model_file, weights_file)
-    CLASS_NAMES = loadClassesNames(join(str(models_dir), 'classesNames'+modelName+'.json'))# np.array(['oneNormB1', 'oneNormB2', 'oneNormBackB1', 'oneNormBackB2', 'oneNormBackM1', 'oneNormBackM2', 'oneNormBackT1', 'oneNormM1', 'oneNormM2', 'oneNormT1'])
+    CLASS_NAMES = loadClassesNames(join(str(models_dir), 'classes'+modelName+'.json'))# np.array(['oneNormB1', 'oneNormB2', 'oneNormBackB1', 'oneNormBackB2', 'oneNormBackM1', 'oneNormBackM2', 'oneNormBackT1', 'oneNormM1', 'oneNormM2', 'oneNormT1'])
 
 def predict(imgDir, imgName):
     img = getImageToPlot(join(str(imgDir), imgName))
@@ -26,4 +28,7 @@ def predict(imgDir, imgName):
 
 if __name__ == '__main__':
     # (path, timeRecording, laserHeight)
-    predict("/home/pi/Scripts", "fooFilledSquared.png")
+    if platform.system() == "Windows":
+        predict("C:/Users/e7470/rowData/testDowglas", "202004011130135360BFilledSquared1.png")
+    else:
+        predict("/home/pi/Scripts", "fooFilledSquared.png")
